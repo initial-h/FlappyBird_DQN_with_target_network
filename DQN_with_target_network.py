1b7589e5cbbdd20919d034e4ab157370f3c1adb8
len <363399935@qq.com>
2018-06-05 10:15:03 +0800

    Initial commit

---
+++ DQN_with_target_network.py
@@ -0,0 +1,350 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 17 10:24:12 2018
+
+@author: len
+"""
+#!/usr/bin/env python
+from __future__ import print_function
+
+import tensorflow as tf
+import tflearn 
+import cv2
+import sys
+sys.path.append("game/")
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+import wrapped_flappy_bird as game
+import random
+import numpy as np
+from collections import deque
+import matplotlib.pylab as plt
+import pygame
+import time
+
+GAME = 'bird' # the name of the game being played for log files
+ACTIONS = 2 # number of valid actions
+GAMMA = 0.99 # decay rate of past observations
+OBSERVE = 10000. # timesteps to observe before training
+EXPLORE = 3000000. # frames over which to anneal epsilon
+FINAL_EPSILON = 0.0001 # final value of epsilon
+INITIAL_EPSILON = 0.1 # starting value of epsilon 探索度，epsilon贪心策略
+REPLAY_MEMORY = 50000 # number of previous transitions to remember
+BATCH = 32 # size of minibatch
+FRAME_PER_ACTION = 1
+
+
+def createNetwork():
+    # input layer
+    s = tf.placeholder("float", [None, 80, 80, 4])
+
+    # hidden layers   
+    h_conv1 = tflearn.conv_2d(s, 32, 8, strides=4, activation='relu',weights_init=tflearn.initializations.truncated_normal(stddev=0.01))  
+    h_pool1 = tflearn.max_pool_2d(h_conv1,2,2)
+
+    h_conv2 = tflearn.conv_2d(h_pool1,64,4,strides=2,activation='relu',weights_init=tflearn.initializations.truncated_normal(stddev=0.01))
+   
+    h_conv3 = tflearn.conv_2d(h_conv2,64,3,strides=1,activation='relu',weights_init=tflearn.initializations.truncated_normal(stddev=0.01))
+
+    h_conv3_flat = tflearn.reshape(h_conv3,[-1,1600])
+    h_fc1 = tflearn.fully_connected(incoming=h_conv3_flat,n_units=512,activation='relu',weights_init=tflearn.initializations.truncated_normal(stddev=0.01))
+
+    readout = tflearn.fully_connected(incoming=h_fc1,n_units=ACTIONS)
+
+    return s, readout
+
+def plot_score(score_list):
+    plt.plot(np.arange(len(score_list)), score_list)
+    plt.ylabel('score')
+    plt.xlabel('game')
+    plt.show()
+    
+def plot_cost(cost_list):
+    plt.plot(np.arange(len(cost_list)), cost_list)
+    plt.ylabel('Cost')
+    plt.xlabel('training steps')
+    plt.show()
+    
+def plot_maxvalue(value_list):
+    plt.plot(np.arange(len(value_list)),value_list)
+    plt.ylabel('max_value')
+    plt.xlabel('training step')
+    plt.show()
+
+score_list = []
+cost_list = []
+value_list = []
+
+def trainNetwork(s,q_values,st,target_q_values,reset_target_network_params,sess,train,acceleration):
+    # define the cost function
+    a = tf.placeholder("float", [None, ACTIONS])#action
+    y = tf.placeholder("float", [None])#Q现实
+    readout_action = tf.reduce_sum(tf.multiply(q_values, a), reduction_indices=1)
+    
+    #readout是在s状态下，采取各个动作的Q值，a是实际采取的动作，
+    # multiply一下再reduce_sum就是网络预测的在状态s下采取动作a的Q值，也就是Q估计
+    #readout is the Q value of each action at state s
+    #the result of multiply and reduce_sum is the Q value from network at state s with action a,that is Q evaluate 
+    cost = tf.reduce_mean(tf.square(y - readout_action))
+    #这里就是Q现实与Q估计的差值
+    #the diffence between Q real and Q evaluate
+    train_step = tf.train.AdamOptimizer(1e-6).minimize(cost)
+
+    # open up a game state to communicate with emulator
+    game_state = game.GameState()
+    game_state.acceleration = acceleration#if acceleration
+    # store the previous observations in replay memory
+    D = deque()
+    cost_tmp = deque(maxlen = 1000)##save a length of cost to judge if we should adjust the step parameter C
+    cost_tmp.append(0)#to avoid np.mean nan
+
+    # get the first state by doing nothing and preprocess the image to 80x80x4
+    do_nothing = np.zeros(ACTIONS)
+    do_nothing[0] = 1
+    x_t, r_0, terminal = game_state.frame_step(do_nothing)#image_data, reward, terminal
+    x_t = cv2.cvtColor(cv2.resize(x_t, (80, 80)), cv2.COLOR_BGR2GRAY)
+    ret, x_t = cv2.threshold(x_t,1,255,cv2.THRESH_BINARY)
+    s_t = np.stack((x_t, x_t, x_t, x_t), axis=2)
+
+    # saving and loading networks
+    saver = tf.train.Saver()
+    
+    sess.run(tf.global_variables_initializer())
+    if not os.path.exists("saved_networks"):
+        os.makedirs("saved_networks")
+    checkpoint = tf.train.get_checkpoint_state("saved_networks")
+    if checkpoint and checkpoint.model_checkpoint_path:
+        saver.restore(sess, checkpoint.model_checkpoint_path)
+        print("Successfully loaded:", checkpoint.model_checkpoint_path)
+    else:
+        print("Could not find old network weights")
+     
+    t = 0
+    score = 0
+    start_time = time.time()#to compute the time to avoid ploting too frequently 
+    ##################################train the network#######################################        
+    if train:
+        sess.run(reset_target_network_params)
+#        print('reset_target_network_params!!!!!!!')
+
+        # start training
+        epsilon = INITIAL_EPSILON
+        game_num = 0
+        C = 1#every C step reset target Q network
+        while "flappy bird" != "angry bird":
+            # choose an action epsilon greedily
+            readout_t = q_values.eval(feed_dict={s : [s_t]})[0]
+            a_t = np.zeros([ACTIONS])
+            action_index = 0
+            if t % FRAME_PER_ACTION == 0:
+                if random.random() <= epsilon:
+#                    print("----------Random Action----------")
+                    action_index = random.randrange(ACTIONS)
+                    a_t[random.randrange(ACTIONS)] = 1
+                else:
+                    action_index = np.argmax(readout_t)
+                    a_t[action_index] = 1
+            else:
+                a_t[0] = 1 # do nothing
+    
+            # scale down epsilon
+            if epsilon > FINAL_EPSILON and t > OBSERVE:
+                epsilon -= (INITIAL_EPSILON - FINAL_EPSILON) / EXPLORE
+    
+            # run the selected action and observe next state and reward
+            x_t1_colored, r_t, terminal = game_state.frame_step(a_t)
+            x_t1 = cv2.cvtColor(cv2.resize(x_t1_colored, (80, 80)), cv2.COLOR_BGR2GRAY)
+            ret, x_t1 = cv2.threshold(x_t1, 1, 255, cv2.THRESH_BINARY)
+            x_t1 = np.reshape(x_t1, (80, 80, 1))
+            #s_t1 = np.append(x_t1, s_t[:,:,1:], axis = 2)
+            s_t1 = np.append(x_t1, s_t[:, :, :3], axis=2)
+    
+            # store the transition in D
+            D.append((s_t, a_t, r_t, s_t1, terminal))
+            if len(D) > REPLAY_MEMORY:
+                D.popleft()
+    
+            # only train if done observing
+            if t > OBSERVE:
+                # sample a minibatch to train on
+                minibatch = random.sample(D, BATCH)
+    
+                # get the batch variables
+                #s_t, a_t, r_t, s_t1
+                s_j_batch = [d[0] for d in minibatch]
+                a_batch = [d[1] for d in minibatch]
+                r_batch = [d[2] for d in minibatch]
+                s_j1_batch = [d[3] for d in minibatch]
+    
+                y_batch = []#Q现实
+                readout_j1_batch = target_q_values.eval(feed_dict = {st : s_j1_batch})
+                #tensorflow还可以这样操作，这个eval可以转变BN吗，可以试试
+                for i in range(0, len(minibatch)):
+                    terminal = minibatch[i][4]
+                    # if terminal, only equals reward
+                    if terminal:
+                        y_batch.append(r_batch[i])
+                    else:
+                        y_batch.append(r_batch[i] + GAMMA * np.max(readout_j1_batch[i]))
+    
+                # perform gradient step
+                train_step.run(feed_dict = {
+                    y : y_batch,
+                    a : a_batch,
+                    s : s_j_batch}
+                )
+                cost_list.append(cost.eval(feed_dict = {y:y_batch,a:a_batch,s:s_j_batch}))
+                cost_tmp.append(cost.eval(feed_dict = {y:y_batch,a:a_batch,s:s_j_batch}))
+                
+            # update the old values
+            s_t = s_t1
+            t += 1
+            
+    #         save progress every 10000 iterations
+            if t % 10000 == 0:
+                saver.save(sess, 'saved_networks/' + GAME + '-dqn', global_step = t)
+                
+            #adjust the step C by cost_tmp
+            if t % C == 0:
+                sess.run(reset_target_network_params)
+#                print('reset_target_network_params!!!!!!!',C)
+                if np.mean(cost_tmp)>0.5:
+                    C*=2
+                else:
+                    C = np.ceil(C/2)
+    
+            # print info
+            state = ""
+            if t <= OBSERVE:
+                state = "observe"
+            elif t > OBSERVE and t <= OBSERVE + EXPLORE:
+                state = "explore"
+            else:
+                state = "train"
+    
+    #        print("TIMESTEP", t, "/ STATE", state, \
+    #            "/ EPSILON", epsilon, "/ ACTION", action_index, "/ REWARD", r_t, \
+    #            "/ Q_MAX %e" % np.max(readout_t))
+            # write info to files
+            '''
+            if t % 10000 <= 100:
+                a_file.write(",".join([str(x) for x in readout_t]) + '\n')
+                h_file.write(",".join([str(x) for x in h_fc1.eval(feed_dict={s:[s_t]})[0]]) + '\n')
+                cv2.imwrite("logs_tetris/frame" + str(t) + ".png", x_t1)
+            '''
+            score+=r_t
+            value_list.append(np.max(readout_t))
+            if terminal:
+                game_state.acceleration = acceleration#if acceleration
+                game_num+=1
+                score_list.append(score)
+                if time.time()-start_time>60:#don't print too frequently
+                    plot_cost(cost_list)
+                    plot_maxvalue(value_list)
+                    plot_score(score_list)
+                    print('---------game:',game_num,state,'train_step:',t,'score:',score)
+                    print('C is:',C,'mean cost:',np.mean(cost_tmp))
+                    start_time = time.time()
+                score=0
+    ##########################test network###############################################3
+    else:
+        terminal = False
+        while not terminal:
+            # choose an action epsilon greedily
+            readout_t = q_values.eval(feed_dict={s : [s_t]})[0]
+            a_t = np.zeros([ACTIONS])
+            action_index = 0
+            if t % FRAME_PER_ACTION == 0:
+                action_index = np.argmax(readout_t)
+                a_t[action_index] = 1
+            else:
+                a_t[0] = 1 # do nothing
+
+            # run the selected action and observe next state and reward
+            x_t1_colored, r_t, terminal = game_state.frame_step(a_t)
+            x_t1 = cv2.cvtColor(cv2.resize(x_t1_colored, (80, 80)), cv2.COLOR_BGR2GRAY)
+            ret, x_t1 = cv2.threshold(x_t1, 1, 255, cv2.THRESH_BINARY)
+            x_t1 = np.reshape(x_t1, (80, 80, 1))
+            #s_t1 = np.append(x_t1, s_t[:,:,1:], axis = 2)
+            s_t1 = np.append(x_t1, s_t[:, :, :3], axis=2)
+            s_t = s_t1
+            
+            score+=r_t
+            if time.time()-start_time>10:
+                print('current score:',score)
+                start_time = time.time()
+            if terminal:
+                print('total score:',score)
+
+
+#for human to play the game 
+###################space key to fly, do nothing to fall down#################################
+#just take a try to feel the game's difficulty
+def play_by_human():
+    game_state = game.GameState()
+    game_state.acceleration = False
+    game_1 = True
+    score = 0
+    
+    start_time = time.time()
+    while game_1:
+        a_t = np.zeros([2])
+        keystate = pygame.key.get_pressed() 
+        if keystate[pygame.K_SPACE]:
+            a_t = np.array([0,1])
+        else:
+            a_t = np.array([1,0])
+        x_t1_colored, r_t, terminal = game_state.frame_step(a_t)
+        score += r_t
+        if time.time()-start_time>10:
+            print('current score:',score)
+        if terminal:
+            print('total score:',score)
+#            print('time:',time.time()-start)
+            game_1 = False              
+
+def playGame(train):
+    sess = tf.InteractiveSession()
+    s, q_network= createNetwork()
+    network_params = tf.trainable_variables()
+    q_values = q_network
+    
+    st, target_q_network = createNetwork()
+    target_network_params = tf.trainable_variables()[len(network_params):]
+    target_q_values = target_q_network
+    
+    reset_target_network_params = \
+        [target_network_params[i].assign(network_params[i])
+         for i in range(len(target_network_params))]  
+    trainNetwork(s, q_values, st,target_q_values,reset_target_network_params, sess,train=train,acceleration=train)
+
+
+#if play_by_human = True, play it by yourself with space key 
+#if train = True, train the network with acceleration(do not use FPS)
+#if train = False, test the network model
+# human_play = False       
+# train = True    
+
+def main():
+    if human_play:  
+        play_by_human()
+    elif train:
+        playGame(train)
+    else:
+        playGame(train)
+# main()
+
+if __name__ == "__main__":
+   import argparse
+   parser = argparse.ArgumentParser()
+   parser.add_argument('--train',type =bool,default = False)
+   parser.add_argument('--human_play',type=bool,default = False)
+   parser.add_argument('--test',type = bool,default = True)
+   
+   args = parser.parse_args()
+   if args.human_play:
+       play_by_human()
+   elif args.train:
+       playGame(train=True)
+   elif args.test:
+       playGame(train=False)
---
+++ README.md
@@ -0,0 +1,84 @@
+
+# FlappyBird in DQN with Target Network
+
+<img src="images/demo.gif" width="250">
+
+## Overview
+
+This repo is based on [yenchenlin/DeepLearningFlappyBird](https://github.com/yenchenlin/DeepLearningFlappyBird)
+
+I do these things on his repo:
+* **add freezing target network in DQN algorithm**
+* **rewrite the network using tflearn,a wrapper of tensorflow (you can also write it only with tensorflow)**
+* **write a function for human to play the game(space key to fly, doing nothing to fall down)**
+## Installation Dependencies:
+* Python3
+* TensorFlow >= 1.1.0
+* tflearn
+* pygame
+* OpenCV-Python
+
+## How to Install
+* my system is win10, linux/mac is a little defferent but easier
+```
+pip install tensorflow
+pip install tflearn
+pip install opencv-python
+```
+  pygame click here:　 [PyGame](https://blog.csdn.net/qq_33166080/article/details/68928563)
+  tensorflow gpu click here:　[Windows10 64位下安装TensorFlow - 官方原生GPU版安装](https://blog.csdn.net/goodshot/article/details/61926838)
+
+## How to Run
+* Play the game by yourself
+```
+python DQN_with_target_network.py --human_play True
+```
+* Test the model
+```
+python DQN_with_target_network.py
+```
+* Continue training
+```
+python DQN_with_target_network.py --train True
+```
+## DQN Algorithm
+This algorithm is on paper [Human-level control through deep reinforcement learning ](https://www.ncbi.nlm.nih.gov/pubmed/25719670)
+
+ <img src="images/algorithm.png" width="650"><br>
+
+* The only difference in this code is that I don't fix the C=10000,and adjust it with the network's cost.
+ 
+ ## Details
+  Environment,network architecture and the training parameter nearly all the same with [yenchenlin/DeepLearningFlappyBird](https://github.com/yenchenlin/DeepLearningFlappyBird)
+  * preprocess
+  <img src="images/preprocess.png" width="450">
+  * network
+   <img src="images/network.png" width="450">
+  * game
+  I cancel the FPS restriction when training so that it can run faster depending on your computer.
+  
+
+## Train Tricks
+We know that RL algorithms always get better performance at the cost of stability, sometimes we can't get a good result even with the same code. It is highly based on the parameters of the algorithm,network and even the random seed. When we update some states' action for a more accurate Q value,other actions may get worse,and this is always unavoidable. So,when we find the pictures as follows, we should be aware that the network may has fallen into a bad circumstance and can't improve anymore.
+* Unbalanced samples
+<img src="images/bad_cost.png" width="450"><img src="images/bad_max_value.png" width="450">
+
+We can see from the cost picture that the network fits well, the max_value picture shows the Q value of the best action has converged and range in a section with very low max Q value(-1~0.1). When you train from scratch, you may encounter with this and it is because of Unbalanced samples. The bird always fly to the top of the game screen and finally bump the pipe in a clumsy way. So, the memory D saves all these data and doesn't konw there is a better way to go further and thinks it's the end state of the MDP problem. In addition,the network updates time after time to get lower and lower Q value, at the same time the best action does not change because of BP algorithm and unbalanced samples,in other words,the update is useless to choice a better action.<br>
+
+For example,a state S has two actions: 0 and 1. In fact,0 is a better action but now with a lower Q value,let's assume that Q(0)=0.2,Q(1)=0.3 here. And unfortunately the memory D now has all data about action 1 with reward -1,so the BP algorithm can only base the action 1's data to update the network. The memory D thinks the action 1's value should be -1,but knows nothing about action 0,so he updates action 1's Q value by amending it to -1,but at the same time action 0's Q value has been changed to -1.1 by chance and is still lower than action 1, so the update is useless to choice a better action and this is the reason caused by BP algorithm and unbalanced samples.<br>
+
+How to solve it? The most stupid but always effective method is restarting it to initialize the parameters(better initialize the network do nothing).There must be some better ways need more study.
+
+## Differences between with/without Target Network
+* [yenchenlin/DeepLearningFlappyBird's](https://github.com/yenchenlin/DeepLearningFlappyBird) code is mainly based on paper [Playing Atari with Deep Reinforcement Learning](https://arxiv.org/abs/1312.5602v1) (without target network),and this repo is based on [Human-level control through deep reinforcement learning ](https://www.ncbi.nlm.nih.gov/pubmed/25719670) (with target network)
+* these two ways are all effective,training speed are all slow(depends on many causes and too hard too say which is faster). 
+* With target network, the network's cost can be control in a lower level and perform more stable and less likely to fluctuate wildly(but this problem still exists)
+
+## Future Work Can Try
+There are still some works may improve the performance
+* Training it for a longer time. This model reaches more than 5,000 score when training about 2 days on 4 million frames with COREi7 and 1080Ti(an enough long time for the bird to fly before bumping the pipe), far less than paper's 50 million frames and 38 days.
+* Memory D is only 50,000 here in total,and in paper it's 1 million most recent frames. A huge memory can save more data to fit a more stable network and also with longer time.
+* Frame-skipping technique. Now the network can take an action at every frame, it's very flexible for the network to adjust its state but also extend action/state space which needs more data to fit,and also will take more time for the network to compute and then output an action. In the original paper, it is said as follow:<br> More precisely, the agent sees and selects actions on every kth frame instead of every frame, and its last action is repeated on skipped frames. Because running the emulator forward for one step requires much less computation than having the agent select an action, this technique allows the agent to play roughly k times more games without significantly increasing the runtime.We use k=4 for all games.
+* We use Adam to update network in this game,and RMSProp is used in original paper. I think they are all okay,but maybe there are some diffenrences?
+* More details are in the following picture,and maybe you can find some more improvements.<br>
+<img src="images/hyperparameters.jpg">
